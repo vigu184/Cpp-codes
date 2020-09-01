7e66527e72a51a739dd1ea0544efbd87fe6efa23
Vignesh Hariharan Iyer <vigneshiyer666666@gmail.com>
2020-09-02 01:21:55 +0530

    Added some comments
    Comments in a program are essential for understanding.

--- array.cpp
+++ array.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
 int marks[]={45,50,28,38};
-int mathsmarks[4];
+int mathsmarks[4]; //Array made 
 
 cout<<marks[0];
 cout<<marks[1];
@@ -12,7 +12,7 @@ cout<<marks[3];
 
 for(int i=0; i<4; i++)
 {
-    cout<<i<<marks[i];
+    cout<<i<<marks[i]; //Output 
 }
    
     return 0;
