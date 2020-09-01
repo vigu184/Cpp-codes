4c70ff69cabe31c2092e16a701e3cf86696f7580
Alakhdeep singh <54256747+Alakhdeepsingh@users.noreply.github.com>
2020-09-01 18:37:18 +0530

    files added

---
+++ callbhyreference.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+    int sum(int a, int b)
+    {
+        int c=a+b;
+        return c;
+
+    }
+    int main() {
+        cout<<sum(5,7);
+return 0; }
+
