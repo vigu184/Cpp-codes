a05799ff0fa4f45118a8a1a6f6a9e55a16fec4dc
Vignesh Hariharan Iyer <vigneshiyer666666@gmail.com>
2020-09-30 23:40:09 +0530

    simple comments
    Explain the relevance of nomenclature of variables, objects and other elements in a program.

--- structure.cpp
+++ structure.cpp
@@ -1,26 +1,28 @@
 #include<iostream>
 using namespace std;
-struct rollex
+struct item
 {
-    /* data */
-    int price;
-    char quality;
-    int weight;
+    int price; // price
+    char quality; // quality
+    int weight; //weight
 };
-int main(){
-    struct rollex alakh;
-    struct rollex jayesh;
-    alakh.price=200000;
-    alakh.quality='q';
-    alakh.weight=70;
-    cout<<alakh.price;
-    cout<<alakh.quality;
-    cout<<alakh.weight;
-    jayesh.price=300000000;
-    jayesh.quality='b';
-    jayesh.weight=65;
-    cout<<jayesh.price;
-    cout<<jayesh.quality;
-    cout<<jayesh.weight;
-return 0; }
+
+int main()
+{
+    struct item obj1; //object 1
+    struct item obj2; //object 2
+    obj1.price=200000;  
+    obj1.quality='q';
+    obj1.weight=70;
+    cout<<obj1.price;
+    cout<<obj1.quality;
+    cout<<obj1.weight;
+    obj2.price=300000000;
+    obj2.quality='b';
+    obj2.weight=65;
+    cout<<obj2.price;
+    cout<<obj2.quality;
+    cout<<obj2.weight;
+return 0; 
+}
 
